a514b6f55d847977dcde529ee1d29f257e13147c
Larry Chan <larrychan@Larrys-Mac-mini.local>
2023-03-08 16:18:34 -0500

    funct: restrict decimal poinht to only one per number

--- javascript/calculator/app.js
+++ javascript/calculator/app.js
@@ -87,6 +87,22 @@ function setSigns() {
     updateDisplay();
 }
 
+function setDot() {
+    // Do nothing if there is no initial number.
+    if (CALCULATOR_STORE.length === 0) return;
+
+    // Check and see if last value is a number
+    let itemList = CALCULATOR_STORE.split(" ");
+    let lastIdx = itemList.length - 1; 
+    let isDotlessNumber = /^[0-9]+$/.test(itemList[lastIdx]);
+
+    // Add dot if existing number does not have preexisting dot
+    if (isDotlessNumber) {
+        CALCULATOR_STORE = CALCULATOR_STORE + this.name;
+    }
+    updateDisplay();
+}
+
 function allClearDisplay() {
     CALCULATOR_STORE = "";
     let screen = document.querySelector("#screen");
@@ -132,6 +148,10 @@ function initializeEvents() {
     // Initialize all signs
     const signList = document.querySelectorAll("#keypads .sign");
     signList.forEach(sign => sign.addEventListener("click", setSigns));
+
+    // Initialize Dot
+    const dotButton = document.querySelector("#keypads .dot");
+    dotButton.addEventListener("click", setDot);
 
     // Calculate
     const equal = document.querySelector("#keypads .equal");
